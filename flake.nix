1d6db8d6dd25ab3ff062470c45e9f948767875c3
Sybrand Aarnoutse <sybrand@neuralcoding.com>
2022-08-13 20:00:27 +0200

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = ''
+    WIP GUI for NixOS
+
+    View documentation (like `man configuration.nix`) and your config (like `nixos-option` or a REPL).
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nixos-druid = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "nixos-druid";
+          version = "0.1.0";
+
+          src = ./.;
+          cargoSha256 = "sha256-tfJnq5jE+QlnthOwVz7munIsIoq8Z+qQrs3AJeqltCs=";
+
+          nativeBuildInputs = with pkgs; [ pkgconfig ];
+          buildInputs = with pkgs; [ gtk3 ];
+          cargoBuildFlags = [ "--bin" "nixos-option-browser" "--bin" "nixos-config-browser" ];
+
+          meta = with pkgs.lib; {
+            description = "WIP GUI for NixOS";
+            homepage = "https://gitlab.science.ru.nl/sybrandaarnoutse/nixos-druid";
+            # TODO: Pick license
+            # license = licenses.;
+            maintainers = with maintainers; [ syberant ];
+          };
+        };
+      in rec {
+        packages.nixos-druid = nixos-druid;
+
+        apps.nixos-option-browser = flake-utils.lib.mkApp {
+          drv = nixos-druid;
+          exePath = "/bin/nixos-option-browser";
+        };
+        apps.nixos-config-browser = flake-utils.lib.mkApp {
+          drv = nixos-druid;
+          exePath = "/bin/nixos-config-browser";
+        };
+
+        packages.default = nixos-druid;
+        apps.default = apps.nixos-option-browser;
+      });
+}
